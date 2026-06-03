a609810e322fb18444db472d25bfac3509bfab06
Harsh yadav <harshyadav23444@gmail.in>
2026-06-03 11:37:03 +0530

    update authentication logic

--- controllers/users.js
+++ controllers/users.js
@@ -14,7 +14,7 @@ module.exports.signup = async (req, res) => {
       if (err) {
         return next(err);
       }
-      req.flash("success", "welcome to wonderlust!");
+      req.flash("success", "welcome to TravelScape!");
       res.redirect("/listings");
     });
   } catch (e) {
@@ -28,7 +28,7 @@ module.exports.renderloginForm = (req, res) => {
 };
 
 module.exports.login = async (req, res) => {
-  req.flash("success", "Welcome back to Wonderlust!");
+  req.flash("success", "Welcome back to TravelScape!");
   let redirectUrl = res.locals.redirectUrl || "/listings";
   res.redirect(redirectUrl);
 };
